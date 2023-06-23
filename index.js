1a05e5db0c7ea50a032ea63aa1d319c1db7c3eae
janellegage <jgagerob@asu.edu>
2023-06-23 11:12:10 -0400

    Added more rows

--- index.js
+++ index.js
@@ -10,15 +10,15 @@ const scoreDisplay = document.querySelector('#score')
 const blockWidth = 100
 const blockHeight = 20
 const ballDiameter = 20
-const boardWidth = 760
+const boardWidth = 780
 const boardHeight = 500
 let xDirection = -2
 let yDirection = 2
 
-const userStart = [230, 10]
+const userStart = [350, 50]
 let currentPosition = userStart
 
-const ballStart = [270, 40]
+const ballStart = [380, 70]
 let ballCurrentPosition = ballStart
 
 let timerId
@@ -36,21 +36,68 @@ class Block {
 
 //all my blocks
 const blocks = [
-  new Block(10, 270),
-  new Block(120, 270),
-  new Block(230, 270),
-  new Block(340, 270),
-  new Block(450, 270),
-  new Block(10, 240),
-  new Block(120, 240),
-  new Block(230, 240),
-  new Block(340, 240),
-  new Block(450, 240),
-  new Block(10, 210),
-  new Block(120, 210),
-  new Block(230, 210),
-  new Block(340, 210),
-  new Block(450, 210),
+  //1st Row
+  new Block(10, 470),
+  new Block(120, 470),
+  new Block(230, 470),
+  new Block(340, 470),
+  new Block(450, 470),
+  new Block(560, 470),
+  new Block(670, 470),
+
+  //2nd row
+  new Block(10, 440),
+  new Block(120, 440),
+  new Block(230, 440),
+  new Block(340, 440),
+  new Block(450, 440),
+  new Block(560, 440),
+  new Block(670, 440),
+
+  //3rd row
+  new Block(10, 410),
+  new Block(120, 410),
+  new Block(230, 410),
+  new Block(340, 410),
+  new Block(450, 410),
+  new Block(560, 410),
+  new Block(670, 410),
+
+  //4th row
+  new Block(10, 380),
+  new Block(120, 380),
+  new Block(230, 380),
+  new Block(340, 380),
+  new Block(450, 380),
+  new Block(560, 380),
+  new Block(670, 380),
+
+  //5th row
+  new Block(10, 350),
+  new Block(120, 350),
+  new Block(230, 350),
+  new Block(340, 350),
+  new Block(450, 350),
+  new Block(560, 350),
+  new Block(670, 350),
+
+  //6th row
+  new Block(10, 320),
+  new Block(120, 320),
+  new Block(230, 320),
+  new Block(340, 320),
+  new Block(450, 320),
+  new Block(560, 320),
+  new Block(670, 320),
+
+  //6th row
+  new Block(10, 290),
+  new Block(120, 290),
+  new Block(230, 290),
+  new Block(340, 290),
+  new Block(450, 290),
+  new Block(560, 290),
+  new Block(670, 290),
 ]
 
 //draw my blocks
